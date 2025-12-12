bfda4d760f99b8cb9bad39644dd61baf1a534d01
Asif Akbar <786asifakbar53@gmail.com>
2025-12-12 11:45:57 +0500

    feat: set up Express app and define API endpoint for massage

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.db.js";
+import app from "./app.js"
 dotenv.config({path : "./.env"});
 
 connectDB();
+
+app.get("/api/massage",(req , res)=>{
+    res.send(massage)
+});
+
+app.listen(process.env.PORT , ()=>{
+    console.log("frontend and backend connection successfully")
+})
