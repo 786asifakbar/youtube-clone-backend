1c695d4aac38afbd8414503686a2c4b95541bd1a
Asif Akbar <786asifakbar53@gmail.com>
2025-12-13 13:43:24 +0500

    fix: correct cors middleware usage and improve request handling feat: add asyncHandler utility for better error management

--- src/app.js
+++ src/app.js
@@ -4,10 +4,15 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors{
-    origin: process.env.CORS_ORIGIN
-})
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials : true
+}));
 
+app.use(express.json({limit : "10mb"}));
+app.use(express.urlencoded({extended : true , limit : "10mb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 
 export default app;
