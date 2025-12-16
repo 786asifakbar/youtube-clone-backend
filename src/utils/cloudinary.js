7fd831f777bb9079f11172ff113978a9cfe78333
Asif Akbar <786asifakbar53@gmail.com>
2025-12-16 18:52:06 +0500

    fix: correct syntax errors in cloudinary upload function

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -11,10 +11,10 @@ cloudinary.config({
 
 const uploadOnCloudinary = async (localFilePath)=>{
  try {
-    if(!localFilePath) return null ,
+    if(!localFilePath) return null
     const response = await cloudinary.uploader.upload
     (localFilePath , {
-    resource_type: "auto";
+    resource_type: "auto"
     })
      console.log("file is uploaded on cloudinary" , response.url)
      return response;
