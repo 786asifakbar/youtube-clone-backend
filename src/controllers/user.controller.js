c8334f01007ff799ce8a7ff8d27111deb469f249
Asif Akbar <786asifakbar53@gmail.com>
2025-12-25 11:17:56 +0500

    fix: comment out unused cover image upload and related code in user registration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -29,7 +29,7 @@ if(
 
 //check for images , check for avatar 
 const avatarlocalPath = req.files?.avatar[0]?.path;
-const coverImageLocalPath = req.files?.coverImage[0]?.path;
+//const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
 if( !avatarlocalPath ){
     throw new ApiError( 400 , " Avatar file is required ")
@@ -38,7 +38,7 @@ if( !avatarlocalPath ){
 
 //upload them on cloundinary
 const avatar = await uploadOnCloudinary(avatarlocalPath)
-const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+//const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
 if(!avatar){
     throw new ApiError( 400 , " Avatar file is required ")
@@ -48,7 +48,7 @@ if(!avatar){
 User.create({
     fullname ,
     avatar : avatar.url,
-    coverImage : coverImage?.url || "",
+    //coverImage : coverImage?.url || "",
     email ,
     password ,
     username:username.toLowerCase()
